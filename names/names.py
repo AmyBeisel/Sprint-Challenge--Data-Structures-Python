d8a85d7e1a2a323ece3c6f7ee5f49f2ea1a1b502
AmyBeisel <35390179+AmyBeisel@users.noreply.github.com>
2020-07-31 20:18:42 -0600

    finished

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BSTNode
 
 start_time = time.time()
 
@@ -12,15 +13,35 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+#runtime: 8.565490007400513 seconds
+
+#Use BST to look for values
+#First, create a BST (put first element as the root)
+bst = BSTNode(names_1[0])
+
+#add all the names from names_1 in the tree
+for name in names_1:
+    bst.insert(name)
+
+#For each name in the second list
+#check if the name is in the tree
+#if True, add the name to the duplicate list #
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# #runtime: 0.1551980972290039 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
