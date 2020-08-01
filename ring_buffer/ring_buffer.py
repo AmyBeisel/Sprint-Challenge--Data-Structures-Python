0e94bec814231a5efeb063149b8d9a8cbac7a584
AmyBeisel <35390179+AmyBeisel@users.noreply.github.com>
2020-07-31 19:50:41 -0600

    finished

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        #create a list to store items
+        self.list_items = []
+        #set the first index
+        self.index = 0
+       
 
     def append(self, item):
-        pass
+        #if the list didn't reach the full capacity
+        # add item to the end of the list
+        if len(self.list_items) != self.capacity:
+            self.list_items.append(item)
+        #if the list reached the full capacity
+        else:
+            #zero index will be filled with item
+            self.list_items[self.index] = item
+        #increase the index and limit the values it may take
+        self.index = (self.index +1) % self.capacity
+
 
     def get(self):
-        pass
+        return self.list_items
